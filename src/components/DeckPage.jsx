d6f18b37e66429e34eac12f8f30b0cae025c9ca6
Robert Liao <rliao29@uwo.ca>
2025-03-13 20:27:44 -0400

    Deck page adjustments

--- src/components/DeckPage.jsx
+++ src/components/DeckPage.jsx
@@ -1,3 +1,4 @@
+import './componentStyles/DeckPage.css'
 import { useParams } from 'react-router-dom';
 
 const DeckPage = () =>{
@@ -5,8 +6,16 @@ const DeckPage = () =>{
     console.log(params)
     return(
         <>
-            <div>
-                <h1>Deck Page</h1>
+            <button id='return-button'>Return</button>
+        
+            <div id='deck-body'>
+                <h1 id='deck-name'>Deck Name</h1>
+                <h3 id='deck-subtitle'><i>Deck Subtitle</i></h3>
+                <h2 id='number-of-cards'>Number of cards:</h2>
+                <div id='deck-button-section'>
+                    <button className='deck-button' id='edit-deck-button'>Edit Deck</button>
+                    <button className='deck-button' id='study-now-button'>Study Now</button>
+                </div>
             </div>
         </>
     )
