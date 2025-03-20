f328c9b7ec80bcc8be55bd5f2a701b17de22e467
Jarrett <jar.boersen@gmail.com>
2025-03-19 22:11:59 -0400

    Mostly working card move functionality

--- src/components/EditDeckCard.jsx
+++ src/components/EditDeckCard.jsx
@@ -1,42 +1,9 @@
 import "./componentStyles/EditDeckCard.css"
-import {useState} from "react";
+
+/* TODO: Change icon for plus and minus buttons*/
+// TODO: Change file and card names
 
 export function EditDeckCard(props) {
-
-    /* TODO: Change icon for plus and minus buttons*/
-
-    const [index, setIndex] = useState(parseInt(props.index));
-
-    const increaseIndex = () => {
-        props.moveCard(index, index + 1);
-
-        setIndex(index + 1);
-    }
-
-    const decreaseIndex = () => {
-        if (index > 0) {
-            props.moveCard(index, index - 1);
-
-            setIndex(index - 1);
-        }
-    }
-
-    const changeIndex = (newIndexString) => {
-        if (newIndexString === "") {
-            setIndex("");
-            return;
-        }
-
-        const newIndex = parseInt(newIndexString) - 1;
-
-        if (!isNaN(newIndex) && newIndex > 0)
-            setIndex(newIndex);
-        else
-            console.log("Invalid index");
-    }
-
-    // TODO: Make index change move cards and make sure this is valid (it was throwing errors in the console)
-
     return (
         <>
             <div className="edit-card">
@@ -48,9 +15,10 @@ export function EditDeckCard(props) {
                 <input type="text" placeholder="Answer" className="text-input" value={props.answer}/>
 
                 <div className="index-container">
-                    <button className="index-buttons" onClick={decreaseIndex}>-</button>
-                    <input type="number" className="index-number-input" value={index + 1} onChange={e => changeIndex(e.target.value)} />
-                    <button className="index-buttons" onClick={increaseIndex}>+</button>
+                    <button className="index-buttons" onClick={() => props.decreaseIndex(props.index)}>-</button>
+                    <input type="number" className="index-number-input" value={props.index + 1}
+                           onChange={e => props.moveCard(props.index, e.target.value)}/>
+                    <button className="index-buttons" onClick={() => props.increaseIndex(props.index)}>+</button>
                 </div>
             </div>
         </>
--- src/components/EditDeckModal.jsx
+++ src/components/EditDeckModal.jsx
@@ -9,34 +9,80 @@ import {useState} from "react";
 
 const EditDeckModal = (props) => {
 
-    const defaultCards = [{"index": 0, "question": "HI?", "answer": "HELLO"}, {"index": 1, "question": "test?", "answer": "123"},
-        {"index": 2, "question": "Anyone home?", "answer": "Yes"}, {"index": 3, "question": "9 + 10?", "answer": "21"}];
+    const defaultCards = [
+        {"index": 0, "question": "HI?", "answer": "HELLO"},
+        {"index": 1, "question": "test?", "answer": "123"},
+        {"index": 2, "question": "Anyone home?", "answer": "Yes"},
+        {"index": 3, "question": "9 + 10?", "answer": "21"}
+    ];
 
     const [cards, setCards] = useState(defaultCards);
 
-    // setCards(defaultCards);
-
     // TODO: Fix being unable to click padding around modal-container to close modal
 
+    // TODO: Prevent changing index at bounds
+
     const addCard = () => {
-        const newIndex = cards.length + 1;
-        const newCard = {"index": newIndex, "question": "New question", "answer": "New answer"};
+        // Create a new card at the end of the deck, with placeholder text
+        const newCard = {"index": cards.length, "question": "New question", "answer": "New answer"};
         setCards([...cards, newCard]);
         console.log(cards);
     }
 
-    const moveCard = (currIndex, newIndex) => {
-        console.log("moving from", currIndex, "to", newIndex);
+    const increaseIndex = (currIndex) => {
+        const newCards = [...cards]; // Make a copy of the array
 
-        const adjustedCards = [...cards];
+        const targetCard = newCards[currIndex + 1];
+        newCards[currIndex + 1] = newCards[currIndex]; // Increase the moving card's index by 1
+        newCards[currIndex + 1].index = currIndex + 1;
 
-        let targetCard = adjustedCards[newIndex];
-        targetCard.index = currIndex;
+        targetCard.index = currIndex; // Change the target card's index to the index of the moved card
+        newCards[currIndex] = targetCard;
 
-        adjustedCards[newIndex] = adjustedCards[currIndex];
-        adjustedCards[currIndex] = targetCard;
+        setCards(newCards);
+    }
 
-        setCards(adjustedCards);
+    const decreaseIndex = (currIndex) => {
+        const newCards = [...cards]; // Make a copy of the array
+
+        const targetCard = newCards[currIndex - 1];
+
+        newCards[currIndex - 1] = newCards[currIndex]; // Decrease the moving card's index by 1
+        newCards[currIndex - 1].index = currIndex - 1;
+
+        targetCard.index = currIndex; // Change the target card's index to the index of the moved card
+        newCards[currIndex] = targetCard;
+
+        setCards(newCards);
+    }
+
+    const moveCard = (currIndex, newIndexString) => {
+        if (newIndexString === "") {
+
+            return;
+        }
+
+        const newIndex = parseInt(newIndexString) - 1; // -1 because the index is +1 as a input value
+
+        console.log("moving from index", currIndex + 1, "to", newIndex + 1);
+
+        if (isNaN(newIndex)) {
+            console.log("Invalid index");
+        } else if (newIndex < 0 || newIndex > cards.length - 1) {
+            console.log(`Index out of range (1 - ${cards.length})`);
+        } else {
+            const adjustedCards = [...cards];
+
+            const targetCard = adjustedCards[newIndex];
+
+            adjustedCards[newIndex] = adjustedCards[currIndex];
+            adjustedCards[newIndex].index = newIndex;
+
+            targetCard.index = currIndex;
+            adjustedCards[currIndex] = targetCard;
+
+            setCards(adjustedCards);
+        }
     }
 
     // TODO: Make card key unique and not the index
@@ -55,7 +101,9 @@ const EditDeckModal = (props) => {
 
                     <div id="cards-container">
                         {cards.map((card, index) => (
-                            <EditDeckCard key={index} index={card.index} question={card.question} answer={card.answer} moveCard={moveCard}></EditDeckCard>
+                            <EditDeckCard key={index} index={card.index} question={card.question}
+                                          answer={card.answer} increaseIndex={increaseIndex}
+                                          decreaseIndex={decreaseIndex} moveCard={moveCard}></EditDeckCard>
                         ))}
                         <AddDeckCard addCard={addCard}></AddDeckCard>
                     </div>
